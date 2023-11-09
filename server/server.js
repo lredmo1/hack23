9f1866f5c81f2b8bc4025ae9d6c81ff958921e0e
LucileTech <lucile.t.tech@gmail.com>
2023-11-08 18:52:29 -0800

    db config and models

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
 const path = require('path');
 const dotenv = require('dotenv').config();
 const express = require('express');
+const mongoose = require("mongoose");
+// require("./config/dbConfig");
+
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const config = require('./../webpack.config');
 const compiler = webpack(config);
 
+// MODELS IMPORT
+const User = require("./models/User.model");
+
 const app = express();
+
+// parse incoming requests with JSON payloads
+app.use(express.json());
 
 // REQUIRED ROUTERS
 
 // REQUIRED CONTROLLERS (IF NEEDED HERE)
 
 const PORT = process.env.PORT || 3001;
-
-// parse incoming requests with JSON payloads
-app.use(express.json());
 
 // parse incoming requests with urlencoded payloads
 app.use(express.urlencoded({ extended: true }));
@@ -35,6 +41,31 @@ app.use(
 // ADD ROUTES HERE
 // ex app.use('/comments', CommentRouter);
 
+// HOME
+
+app.get("/", async (req, res) => {
+  return res.json({ message: "Hello, World ✌️" });
+});
+
+app.get('/hello', (req, res) => {
+  res.send('hello world');
+});
+
+// USER ROUTES
+
+app.get("/users", async (req, res) => {
+  const allUsers = await User.find();
+  return res.status(200).json(allUsers);
+});
+
+app.post("/user", async (req, res) => {
+  const newUser = new User({ ...req.body });
+  const insertedUser = await newUser.save();
+  return res.status(201).json(insertedUser);
+})
+
+
+
 // serve 404 status
 app.use((req, res) => res.sendStatus(404));
 
@@ -50,4 +81,18 @@ app.use((err, req, res, next) => {
   res.status(errorObj.status).send(errorObj.message);
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://localhost:27017/gdi2023"
+    );
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+start();
+
