e0f86a6c0d06b65942b7c41dd8c97e1a1b81b967
Meredith Britt - Mac <118227496+mfrazb@users.noreply.github.com>
2023-11-08 20:12:22 -0500

    Add middleware to parse incoming JSON or urlencoded payloads and comments for where to add routers in later

--- server/server.js
+++ server/server.js
@@ -3,28 +3,51 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-
-const app = express();
 const config = require('./../webpack.config');
 const compiler = webpack(config);
+
+const app = express();
+
+// REQUIRED ROUTERS
+
+// REQUIRED CONTROLLERS (IF NEEDED HERE)
 
 const PORT = process.env.PORT || 3001;
 
 // parse incoming requests with JSON payloads
 app.use(express.json());
 
+// parse incoming requests with urlencoded payloads
+app.use(express.urlencoded({ extended: true }));
+
+// serve static files in production mode
 if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, './../dist')));
 }
 
+// serve files on publicPath specified in webpack config
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
   }),
 );
 
-app.get('/hello', (req, res) => {
-  res.send('hello world');
+// ADD ROUTES HERE
+// ex app.use('/comments', CommentRouter);
+
+// serve 404 status
+app.use((req, res) => res.sendStatus(404));
+
+// global error handling
+app.use((err, req, res, next) => {
+  const defaultError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultError, err);
+  console.log(errorObj.log);
+  res.status(errorObj.status).send(errorObj.message);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
