437faa746632616d9117f8b92bdabcad7df02897
Kasandra Wolf <kwolf@rdgfilings.com>
2023-11-10 16:53:31 -0700

    Added HTML text to OneTeachingText

--- client/components/OneTeachingText.js
+++ client/components/OneTeachingText.js
@@ -3,19 +3,265 @@ import DiscussionThreads from '../containers/DiscussionThreads'
 
 function OneTeachingText() {
   return (
-	<>
-		<div>
-			How to Write a Story: A Complete Step-By-Step Guide to Writing a Good Story
-			Step 1: Determine Your Setting
-			Location is an enormously useful tool in novel-building. You should treat it as you would treat a character, allowing it to convey mood and letting it reveal more of itself over time. By selecting locations that excite you, you can transform relatively mundane scenes into more compelling ones. Your enthusiasm will come through in your writing, and your characters will view and interact with your locales in a more engaged way.
+    <>
+      <div>
+        <h2>
+          <strong>
+            How to Write a Story: A Complete Step-By-Step Guide to Writing a Good
+            Story
+          </strong>
+        </h2>
+        <h3>
+          <strong>Step 1: Determine Your Setting</strong>
+        </h3>
+        <p>
+          Location is an enormously useful tool in novel-building. You should treat it
+          as you would treat a character, allowing it to convey mood and letting it
+          reveal more of itself over time. By selecting locations that excite you, you
+          can transform relatively mundane scenes into more compelling ones. Your
+          enthusiasm will come through in your writing, and your characters will view
+          and interact with your locales in a more engaged way.
+        </p>
+        <p>
+          Location can also provide the inspiration for scenes and can even shape the
+          course of your story. All the research you conducted in the first phase of
+          writing will come in handy during your first draft, when you find yourself
+          needing to describe a particular street, park, or other scene set somewhere
+          previously unfamiliar to you.
+        </p>
+        <p>
+          To weave setting into your story, ask yourself the following questions, then
+          answer them in your prose.
+        </p>
+        <ul>
+          <li>
+            What season is your novel set in? What is the weather like in your
+            location at that time?
+          </li>
+          <li>
+            What are the elements of your setting like during this time? For example,
+            was that business closed fifteen years ago? Did that street have a
+            different name then? Was that statue erected before your characters
+            arrived there?
+          </li>
+          <li>
+            Does your novel center around a major world event, like war or natural
+            disaster? How does this limit or define your time frame and impact your
+            setting?
+          </li>
+          <li>
+            What cultural details belong to this time and location? Consider music,
+            literature, entertainment, clothing styles, food trends, lifestyle trends,
+            and big national events or crises that shape public sentiments.
+          </li>
+        </ul>
+        <h3>
+          <strong>Step 2: Make Memorable Characters</strong>
+        </h3>
+        <p>
+          Character and event are inseparable, because a person is what happens to
+          them. You might think of this as a distinction from films, where actors are
+          cast into pre-existing roles. But a novel is a character interacting with
+          events over time.
+        </p>
+        <p>
+          Your job as a writer is to learn about your character by observing how they
+          interact with the world around them. Characters—like real people—have
+          hobbies, pets, histories, ruminations, and obsessions. It's essential to
+          your novel that you understand these aspects of your character so that you
+          are equipped to understand how they may react under the pressures of events
+          they encounter.
+        </p>
+        <p>
+          If you're feeling stuck or are seeking a direction to take your characters
+          in,{" "}
+          <a href='\"https://www.masterclass.com/articles/writing-tips-for-character-development\"'>
+            consult this guide on character development
+          </a>
+          .
+        </p>
+        <h3>
+          <strong>Step 3: Understand the 2 Types of Conflict</strong>
+        </h3>
+        <p>
+          Every story is made up of both events and characters. A story happens
+          because a pattern is interrupted. If you are writing about a day that is
+          like any other day, it is most likely a routine, not a story. In order to be
+          a story, something has to happen. We call what happens in a novel the plot.
+        </p>
+        <p>There are two types of conflict:</p>
+        <ol>
+          <li>1. Internal conflict (a threat from within)</li>
+          <li>2. External conflict (a threat from outside)</li>
+        </ol>
+        <p>
+          Both types of conflict create tension in a narrative and help move the story
+          forward. Conflict drives character development as well as plot. Conflict
+          also adds layers to your story. Your main character can face an external
+          conflict like destroying a sworn enemy while also battling a more subtle,
+          internal conflict: her vow towards pacifism. Your plot will develop
+          naturally if you give your character a motivation, then throw obstacles in
+          her way.
+        </p>
+        <p>
+          It might be a good idea to test out both types of conflict before you settle
+          on one. Why not write a short story that uses both types of conflict, and
+          then decide which one works best for you?
+        </p>
+        <p>
+          No matter what combination of events you knit together to make the plot,
+          each should be compelling and significant enough to pull your reader into
+          the story and make them wonder what will happen next.
+        </p>
+        <h3>
+          <strong>Step 4: Give Your Plot a Twist</strong>
+        </h3>
+        <p>
+          Any good story will include a few plot twists and red herrings. Read on to
+          find out more.
+        </p>
+        <ul>
+          <li>
+            Include at least two or three twists in your story. These help keep
+            readers engaged, especially in the middle of your book when your plot
+            might otherwise start to drag. Carrying readers through the middle of a
+            story is challenging, and there needs to be enough excitement to keep them
+            reading to the end. A great twist will surprise the reader and turn their
+            whole understanding of the story on its head.{" "}
+            <a href='\"https://www.masterclass.com/articles/everything-you-need-to-know-about-writing-plot\"'>
+              For more tips on story plot, check out this comprehensive guide to plot
+              here
+            </a>
+            .
+          </li>
+          <li>
+            Trick your readers by planting \"false leads.\" Also known as \"red
+            herrings,\" these are details added to purposefully mislead people and
+            prevent them from predicting an outcome. While adult mysteries are filled
+            with carefully hidden clues, children's horror novels should be packed
+            with tricks to lead kids astray and thereby surprise them even more when
+            something (like the true identity of a monster) is revealed. Read more
+            about{" "}
+            <a href='\"https://www.masterclass.com/articles/what-is-a-red-herring-in-writing-definition-of-red-herring-with-examples\"'>
+              red herrings and false leads here
+            </a>
+            .
+          </li>
+          <li>
+            A \"cliffhanger\" is a device that compels readers to find out what
+            happens next in a story. Writing great cliffhangers is key to making your
+            book a page-turner and it's one of the easiest ways to make your writing
+            more suspenseful. Some writers might feel it to be a \"cheap trick\" or an
+            easy gimmick, but it's a tried and true way to get people to read—and keep
+            them reading.{" "}
+            <a href='\"https://www.masterclass.com/articles/how-to-write-a-cliffhanger-14-tips-for-writing-page-turning-cliffhangers-with-dan-brown-and-rl-stine\"'>
+              Read more about cliffhangers and how to write them in this guide
+            </a>
+            .
+          </li>
+        </ul>
+        <h3>
+          <strong>Step 5: Recreate Natural Dialogue</strong>
+        </h3>
+        <p>
+          In real life, speech has lots of padding or \"stuffing\": words like{" "}
+          <i>umms</i> and <i>yeahs</i>. But dialogue in fiction must be both more
+          incisive and selective. It is shorn down to reveal what people want from one
+          another, reveal character, and dramatize power struggles.
+        </p>
+        <ul>
+          <li>
+            When your characters are speaking, they should be trying to get something
+            from one another, or make a power play. (Seduction is one form of power
+            play.) As you draft each scene, ask yourself what your characters are
+            trying to get. What are they trying to avoid? How do these wants inflect
+            their speech and guide what they say—or don't say?
+          </li>
+          <li>
+            There are often wide gaps between what people say and what they are
+            thinking, between what one understands and what one refuses to hear. These
+            gaps can collectively be referred to as <i>subtext</i>, and they are
+            valuable territory for the fiction writer. Stay alert to a character's
+            thoughts, and let them generate drama in the scenes you write.
+          </li>
+          <li>
+            To get dialogue right, you must understand how your characters speak. This
+            is likely influenced by where they come from, their social class,
+            upbringing, and myriad other factors. Speech and tone are always bound up
+            in what has happened and is happening to a character.
+            <br />
+            <br />
+            &nbsp;
+          </li>
+          <li>
+            If you are setting your story in the past, your dialogue should accurately
+            reflect idioms and speech patterns of the period. Words, like clothes, go
+            in and out of style. Conversations need to be specific to the time you're
+            writing in without seeming contrived.
+          </li>
+        </ul>
+        <h3>
+          <strong>Step 6: Articulate Voice Through Point of View</strong>
+        </h3>
+        <p>
+          One way to determine what point of view strategy to use in your novel is to
+          ask: whose voice is telling the story? To whom are they telling it, and why?
+          Common point of view strategies include:
+        </p>
+        <ul>
+          <li>
+            <strong>First person point of view</strong>. This is the \"I.\"
+          </li>
+          <li>
+            <a href='\"https://masterclass.com/articles/tips-for-writing-third-person-limited-point-of-view\"'>
+              <strong>Third person limited</strong>
+            </a>
+            <strong> point of view</strong>. This is the \"he\" or \"she.\"
+          </li>
+          <li>
+            <strong>Third person omniscient point of view</strong>, in which a
+            narrator who is not a character and who knows more than the characters
+            relays the events to the reader.
+          </li>
+          <li>
+            <strong>Second person point of view</strong>, which is structured around
+            the \"you\" pronoun, and is less common in novel-length work but can work
+            well for short stories.
+          </li>
+        </ul>
+        <p>
+          You don't have to be tied to one point of view throughout your short story
+          or your novel; some bestselling novels move from first to third or first to
+          second. Let your material guide your decision.
+        </p>
+        <p>
+          The only way to decide the best point of view strategy for your novel is to
+          try different ones. Likely, you'll know the right one for your story because
+          the writing will begin to move more quickly, and you'll feel momentum.
+        </p>
+        <p>
+          Point of view strategy is deeply bound up with what story you want to tell
+          and will guide how that story unspools. So no matter where you are in the
+          drafting process, devote some time to thinking through the risks and rewards
+          of different POV strategies and consider who in your story may be best
+          suited to hold the narrative reins.{" "}
+          <a href='\"https://www.masterclass.com/articles/complete-guide-to-point-of-view-in-writing-definitions-and-examples\"'>
+            Learn more about point of view strategy here
+          </a>
+          .
+        </p>
+        <p>
+          There's no right or wrong way to write a story. Good fiction writing takes a
+          lot of imagination, time, and practice on your part as a writer. Even the
+          best writers practice writing different stories, just like athletes are
+          constantly training at the gym. The first time can be hard, but it gets
+          easier with practice. Work steadily, and if getting published is your goal,
+          stay focused and keep developing your skills. You'll get there!
+        </p>
 
-			Location can also provide the inspiration for scenes and can even shape the course of your story. All the research you conducted in the first phase of writing will come in handy during your first draft, when you find yourself needing to describe a particular street, park, or other scene set somewhere previously unfamiliar to you.
-
-			To weave setting into your story, ask yourself the following questions, then answer them in your prose.
-
-		</div>
-		<DiscussionThreads lessonId={"654d55290487eb9c970237ae"} />	
-	</>
+      </div>
+      <DiscussionThreads lessonId={"654d55290487eb9c970237ae"} />
+    </>
   )
 }
 
