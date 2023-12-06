42cd392c2b19c010e7924aae3aace91c863069f8
Lisa Primeaux-Redmond <redlisa@gmail.com.com>
2023-12-05 14:22:38 -1000

    clean up app, footer, navbar

--- client/App.jsx
+++ client/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./containers/Home";
 import UserProfile from "./containers/UserProfile";
 import AllLessons from "./containers/AllLessons";
-import Lesson from "./containers/Lesson";
 import UploadContent from "./containers/UploadContent";
 import About from "./containers/About";
 import NavBar from "./components/Navbar";
@@ -15,32 +14,32 @@ import "./stylesheets/App.scss";
 
 const App = () => {
 
-  return (
+	return (
 
-    <BrowserRouter>
+		<BrowserRouter>
 
-      <NavBar></NavBar>
+			<NavBar></NavBar>
 
-      <Routes>
+			<Routes>
 
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-		{/* TODO login */}
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/learn" element={<AllLessons />} />
-		{/* TODO page for uploading written lesson */}
-		<Route path="/create-video-lesson" element={<UploadContent />} />
-		{/* TODO view a written lesson text-lesson/:id */}
-        <Route path="/singlelesson" element={<OneTeachingText />} />
-		{/* TODO view a video lesson video-lesson/:id */}
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				{/* TODO login */}
+				<Route path="/profile" element={<UserProfile />} />
+				<Route path="/learn" element={<AllLessons />} />
+				{/* TODO page for uploading written lesson */}
+				<Route path="/create-video-lesson" element={<UploadContent />} />
+				{/* TODO view a written lesson text-lesson/:id */}
+				<Route path="/singlelesson" element={<OneTeachingText />} />
+				{/* TODO view a video lesson video-lesson/:id */}
 
-      </Routes>
+			</Routes>
 
-      <Footer></Footer>
+			<Footer></Footer>
 
-    </BrowserRouter>
+		</BrowserRouter>
 
-  );
+	);
 
 };
 
