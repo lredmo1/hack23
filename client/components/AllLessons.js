84d19451899ab26f636735ef0c585ba9a990d236
Lisa Primeaux-Redmond <redlisa@gmail.com.com>
2023-12-05 15:34:33 -1000

    clean up all lessons page

--- client/components/AllLessons.js
+++ client/components/AllLessons.js
@@ -1,94 +1,108 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
+
 import LessonText from './LessonText';
-import { Container, Grid } from '@mui/material';
 import LessonVideo from './LessonVideo';
 
 function AllLessons() {
-	const[lessons, setLessons] = useState([]); 
-	const [videos, setVideos] = useState([]); 
+	
+	const[textLessons, setTextLessons] = useState([]); 
+	const [videoLessons, setVideoLessons] = useState([]); 
 
-//   useEffect(() => {
+// 	TODO update with new back end
+//  useEffect(() => {
 // 		// Fetch All lessons from DB
 // 		fetch("http://localhost:3000/api/teaching-texts/")
 // 			.then((resp) =>
 // 			resp.json())
 // 			.then((res) => {
 //         // const list = res.concat(res).concat(res); // not a lot of data to read right now so duplicating
-// 				setLessons(res);
+// 				setTextLessons(res);
 // 			});
 //       fetch("http://localhost:3000/api/video-uploads/")
 //         .then((resp) => resp.json())
 //         .then((res) => {
 //           // not a lot of data to read right now so duplicating
-//           setVideos(res);
+//           setVideoLessons(res);
 //         });
 
 // 	}, []);
 
-	const fakeLesson = {
-		title: 'Test Lesson',
-        description: 'This is a lesson description',
-        _id: 123
-	}
+	const fakeTextLessons = [
+		{
+			title: 'Text Lesson 1',
+			description: 'This is a lesson description',
+			_id: 123
+		},
+		{
+			title: 'Text Lesson 2',
+			description: 'This is a lesson description',
+			_id: 123
+		}
+	]
 
-  console.log(videos);
-  return (
-    <>
-    <div className="all-lessons">
-      <div>
-        <h1>Available Lessons</h1>
-        <Grid
-          rowSpacing={4}
-          columnSpacing={3}
-          container
-          direction="row"
-          alignItems="stretch"
-        >
-          {/* {lessons.map((lesson, index) => {
-            console.log(lesson);
-            return ( */}
-              <Grid item 
-			//   key={index} 
-			  xs={3}>
-                 <Link to={`/singlelesson`}>
-                <LessonText
-                  title={fakeLesson.title}
-                  desc={fakeLesson.description}
-                  id={fakeLesson._id}
-                ></LessonText>
-              </Link>
-              </Grid>
-            {/* ); */}
-          {/* })} */}
-        </Grid>
-      </div>
-      <div>
-        <h1>Available Videos</h1>
-        <Grid
-          rowSpacing={4}
-          columnSpacing={3}
-          container
-          direction="row"
-          alignItems="stretch"
-        >
-          {videos.map((singleVideo, index) => {
-            
-            return (
-              <Grid item key={index} xs={3}>
-                <LessonVideo
-                  caption={singleVideo.caption}
-                  video={singleVideo.video}
-                  id={singleVideo._id}
-                ></LessonVideo>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </div>
-    </div>
-    </>
-  );
+	const fakeVideoLessons = [
+		{
+			caption: 'Video Lesson 1',
+			video: 'This is a lesson description',
+			id: 123
+		},
+		{
+			caption: 'Video Lesson 1',
+			video: 'This is a lesson description',
+			id: 456
+		}
+	]
+
+	return (
+
+		<>
+
+			<div className="all-lessons">
+
+				<div>
+
+					<h1>Text Lessons</h1>
+
+					{fakeTextLessons.map((textLesson, index) => {
+						return (
+						// TODO update to link to display of text lessons
+						<Link to={`/singlelesson`} key={index}>
+							<LessonText
+								key={index}
+								title={textLesson.title}
+								desc={textLesson.description}
+								id={textLesson._id}>
+							</LessonText>
+						</Link>
+						);
+					})}
+
+				</div>
+
+				<div>
+
+					<h1>Video Lessons</h1>
+					
+					{fakeVideoLessons.map((videoLesson, index) => {
+						return (
+							<LessonVideo
+								key={index}
+								caption={videoLesson.caption}
+								video={videoLesson.video}
+								id={videoLesson._id}>
+							</LessonVideo>
+						);
+					})}
+
+				</div>
+
+			</div>
+
+		</>
+
+	);
+
 }
 
 export default AllLessons
